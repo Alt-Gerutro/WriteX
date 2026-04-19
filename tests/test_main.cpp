903f94236163a15b0c20ef5d22368c7cc2dff790
Ger <gerutrogame@gmail.com>
2026-04-19 18:51:17 +0300

    Added more tests.

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,118 +1,309 @@
 #include <catch2/catch_test_macros.hpp>
+#include <sstream>
+#include <utility>
+#include <vector>
 #include <writex.hpp>
+#include <string>
 
-WriteX l("Format", "file.log");
+static std::ostringstream fake_out;
+
+WriteX l("Global", fake_out);
 
 TEST_CASE("Format case", "[format]") {
   std::string oldFmt = l.getFormat();
-  l.setFormat("%N %L %M %F %f %l %% %o");
-  CHECK(l.getFormat() == "%N %L %M %F %f %l %% %o");
 
-  CHECK(l.format_msg("Hello {}", "World!") == "Hello World!");
-  CHECK(l.format_msg("Hellosths {}sth", "World!") == "Hellosths World!sth");
+  struct FormatTest {
+    std::string format;
+    std::vector<std::pair<WriteX_Level, std::string>> ex;
+  };
 
-  CHECK(l.format(WriteX_Level::DEBUG, "MESSAGE", "FILE", "FUNC", 1) == "Format DEBUG MESSAGE FILE FUNC 1 % %o");
-  CHECK(l.format(WriteX_Level::INFO, "MESSAGE1", "FILE1", "FUNC1", 2) == "Format INFO MESSAGE1 FILE1 FUNC1 2 % %o");
-  CHECK(l.format(WriteX_Level::WARNING, "MESSAGE2", "FILE2", "FUNC2", 3) == "Format WARNING MESSAGE2 FILE2 FUNC2 3 % %o");
-  CHECK(l.format(WriteX_Level::ERROR, "MESSAGE3", "FILE3", "FUNC3", 4) == "Format ERROR MESSAGE3 FILE3 FUNC3 4 % %o");
-  CHECK(l.format(WriteX_Level::FATAL, "MESSAGE4", "FILE4", "FUNC4", 5) == "Format FATAL MESSAGE4 FILE4 FUNC4 5 % %o");
-  CHECK(l.format(WriteX_Level(1882), "MESSAGE5", "FILE5", "FUNC5", 6) == "Format UNKNOWN MESSAGE5 FILE5 FUNC5 6 % %o");
+  auto testFormat = [&](const FormatTest& ft) {
+    l.setFormat(ft.format);
+    CHECK(l.getFormat() == ft.format);
+    for (const auto& [lvl, ex] : ft.ex) {
+      INFO("Level: " << l.levelToString(lvl));
+      CHECK(l.format(lvl, "MESSAGE", "FILE", "FUNC", 1) == ex);
+    }
+  };
 
-  l.setFormat("%N");
-  CHECK(l.getFormat() == "%N");
-  CHECK(l.format(WriteX_Level::DEBUG, "MESSAGE", "FILE", "FUNC", 1) == "Format");
-  CHECK(l.format(WriteX_Level::INFO, "MESSAGE1", "FILE1", "FUNC1", 2) == "Format");
-  CHECK(l.format(WriteX_Level::WARNING, "MESSAGE2", "FILE2", "FUNC2", 3) == "Format");
-  CHECK(l.format(WriteX_Level::ERROR, "MESSAGE3", "FILE3", "FUNC3", 4) == "Format");
-  CHECK(l.format(WriteX_Level::FATAL, "MESSAGE4", "FILE4", "FUNC4", 5) == "Format");
-  CHECK(l.format(WriteX_Level(842), "MESSAGE5", "FILE5", "FUNC5",6) == "Format");
+  SECTION("All format specifiers") {
+    testFormat({
+      "%N %L %M %F %f %l %% %0 %o %",
+      {
+        {WriteX_Level::DEBUG, "Global DEBUG MESSAGE FILE FUNC 1 % %0 %o %"},
+        {WriteX_Level::INFO, "Global INFO MESSAGE FILE FUNC 1 % %0 %o %"},
+        {WriteX_Level::WARNING, "Global WARNING MESSAGE FILE FUNC 1 % %0 %o %"},
+        {WriteX_Level::ERROR, "Global ERROR MESSAGE FILE FUNC 1 % %0 %o %"},
+        {WriteX_Level::FATAL, "Global FATAL MESSAGE FILE FUNC 1 % %0 %o %"},
+        {WriteX_Level(123), "Global UNKNOWN MESSAGE FILE FUNC 1 % %0 %o %"},
+      }
+    });
+
+    testFormat({
+      "%N",
+      {
+        {WriteX_Level::DEBUG, "Global"},
+        {WriteX_Level::INFO, "Global"},
+        {WriteX_Level::WARNING, "Global"},
+        {WriteX_Level::ERROR, "Global"},
+        {WriteX_Level::FATAL, "Global"},
+        {WriteX_Level(123), "Global"},
+      }
+    });
+
+    testFormat({
+      "%L",
+      {
+        {WriteX_Level::DEBUG, "DEBUG"},
+        {WriteX_Level::INFO, "INFO"},
+        {WriteX_Level::WARNING, "WARNING"},
+        {WriteX_Level::ERROR, "ERROR"},
+        {WriteX_Level::FATAL, "FATAL"},
+        {WriteX_Level(123), "UNKNOWN"},
+      }
+    });
+
+    testFormat({
+      "%M",
+      {
+        {WriteX_Level::DEBUG, "MESSAGE"},
+        {WriteX_Level::INFO, "MESSAGE"},
+        {WriteX_Level::WARNING, "MESSAGE"},
+        {WriteX_Level::ERROR, "MESSAGE"},
+        {WriteX_Level::FATAL, "MESSAGE"},
+        {WriteX_Level(123), "MESSAGE"},
+      }
+    });
+
+    testFormat({
+      "%F",
+      {
+        {WriteX_Level::DEBUG, "FILE"},
+        {WriteX_Level::INFO, "FILE"},
+        {WriteX_Level::WARNING, "FILE"},
+        {WriteX_Level::ERROR, "FILE"},
+        {WriteX_Level::FATAL, "FILE"},
+        {WriteX_Level(123), "FILE"},
+      }
+    });
+
+    testFormat({
+      "%f",
+      {
+        {WriteX_Level::DEBUG, "FUNC"},
+        {WriteX_Level::INFO, "FUNC"},
+        {WriteX_Level::WARNING, "FUNC"},
+        {WriteX_Level::ERROR, "FUNC"},
+        {WriteX_Level::FATAL, "FUNC"},
+        {WriteX_Level(123), "FUNC"},
+      }
+    });
+
+    testFormat({
+      "%l",
+      {
+        {WriteX_Level::DEBUG, "1"},
+        {WriteX_Level::INFO, "1"},
+        {WriteX_Level::WARNING, "1"},
+        {WriteX_Level::ERROR, "1"},
+        {WriteX_Level::FATAL, "1"},
+        {WriteX_Level(123), "1"},
+      }
+    });
+
+    testFormat({
+      "%%",
+      {
+        {WriteX_Level::DEBUG, "%"},
+        {WriteX_Level::INFO, "%"},
+        {WriteX_Level::WARNING, "%"},
+        {WriteX_Level::ERROR, "%"},
+        {WriteX_Level::FATAL, "%"},
+        {WriteX_Level(123), "%"},
+      }
+    });
+
+    testFormat({
+      "%o",
+      {
+        {WriteX_Level::DEBUG, "%o"},
+        {WriteX_Level::INFO, "%o"},
+        {WriteX_Level::WARNING, "%o"},
+        {WriteX_Level::ERROR, "%o"},
+        {WriteX_Level::FATAL, "%o"},
+        {WriteX_Level(123), "%o"},
+      }
+    });
+
+    testFormat({
+      "%0",
+      {
+        {WriteX_Level::DEBUG, "%0"},
+        {WriteX_Level::INFO, "%0"},
+        {WriteX_Level::WARNING, "%0"},
+        {WriteX_Level::ERROR, "%0"},
+        {WriteX_Level::FATAL, "%0"},
+        {WriteX_Level(123), "%0"},
+      }
+    });
+
+    testFormat({
+      "%",
+      {
+        {WriteX_Level::DEBUG, "%"},
+        {WriteX_Level::INFO, "%"},
+        {WriteX_Level::WARNING, "%"},
+        {WriteX_Level::ERROR, "%"},
+        {WriteX_Level::FATAL, "%"},
+        {WriteX_Level(123), "%"},
+      }
+    });
+
+    testFormat({
+      "",
+      {
+        {WriteX_Level::DEBUG, ""},
+        {WriteX_Level::INFO, ""},
+        {WriteX_Level::WARNING, ""},
+        {WriteX_Level::ERROR, ""},
+        {WriteX_Level::FATAL, ""},
+        {WriteX_Level(123), ""},
+      }
+    });
+  }
+
+  l.setFormat(oldFmt);
+}
+
+TEST_CASE("Filter case", "[filter]") {
+  std::string oldFmt = l.getFormat();
+  short oldFil = l.getFilretInt();
+  l.switchNewLine();
 
   l.setFormat("%L");
-  CHECK(l.getFormat() == "%L");
-  CHECK(l.format(WriteX_Level::DEBUG, "MESSAGE", "FILE", "FUNC", 1) == "DEBUG");
-  CHECK(l.format(WriteX_Level::INFO, "MESSAGE1", "FILE1", "FUNC1", 2) == "INFO");
-  CHECK(l.format(WriteX_Level::WARNING, "MESSAGE2", "FILE2", "FUNC2", 3) == "WARNING");
-  CHECK(l.format(WriteX_Level::ERROR, "MESSAGE3", "FILE3", "FUNC3", 4) == "ERROR");
-  CHECK(l.format(WriteX_Level::FATAL, "MESSAGE4", "FILE4", "FUNC4", 5) == "FATAL");
-  CHECK(l.format(WriteX_Level(325), "MESSAGE5", "FILE5", "FUNC5",6) == "UNKNOWN");
+  
+  struct FilterTest {
+    short filter;
+    std::vector<std::pair<WriteX_Level, bool>> outcomes;
+  };
 
-  l.setFormat("%M");
-  CHECK(l.getFormat() == "%M");
-  CHECK(l.format(WriteX_Level::DEBUG, "MESSAGE", "FILE", "FUNC", 1) == "MESSAGE");
-  CHECK(l.format(WriteX_Level::INFO, "MESSAGE1", "FILE1", "FUNC1", 2) == "MESSAGE1");
-  CHECK(l.format(WriteX_Level::WARNING, "MESSAGE2", "FILE2", "FUNC2", 3) == "MESSAGE2");
-  CHECK(l.format(WriteX_Level::ERROR, "MESSAGE3", "FILE3", "FUNC3", 4) == "MESSAGE3");
-  CHECK(l.format(WriteX_Level::FATAL, "MESSAGE4", "FILE4", "FUNC4", 5) == "MESSAGE4");
-  CHECK(l.format(WriteX_Level(102), "MESSAGE5", "FILE5", "FUNC5",6) == "MESSAGE5");
+  std::vector<FilterTest> tests = {
+    {TO_INFO, {
+    {WriteX_Level::DEBUG, false},
+    {WriteX_Level::INFO, true},
+    {WriteX_Level::WARNING, true},
+    {WriteX_Level::ERROR, true},
+    {WriteX_Level::FATAL, true}
+    }},
+    {TO_WARNING, {
+    {WriteX_Level::DEBUG, false},
+    {WriteX_Level::INFO, false},
+    {WriteX_Level::WARNING, true},
+    {WriteX_Level::ERROR, true},
+    {WriteX_Level::FATAL, true}
+    }},
+    {TO_ERROR, {
+    {WriteX_Level::DEBUG, false},
+    {WriteX_Level::INFO, false},
+    {WriteX_Level::WARNING, false},
+    {WriteX_Level::ERROR, true},
+    {WriteX_Level::FATAL, true}
+    }},
+    {TO_ERROR_ASC, {
+    {WriteX_Level::DEBUG, true},
+    {WriteX_Level::INFO, true},
+    {WriteX_Level::WARNING, true},
+    {WriteX_Level::ERROR, true},
+    {WriteX_Level::FATAL, false}
+    }},
+    {TO_WARNING_ASC, {
+    {WriteX_Level::DEBUG, true},
+    {WriteX_Level::INFO, true},
+    {WriteX_Level::WARNING, true},
+    {WriteX_Level::ERROR, false},
+    {WriteX_Level::FATAL, false}
+    }},
+    {TO_INFO_ASC, {
+    {WriteX_Level::DEBUG, true},
+    {WriteX_Level::INFO, true},
+    {WriteX_Level::WARNING, false},
+    {WriteX_Level::ERROR, false},
+    {WriteX_Level::FATAL, false}
+    }},
+    {static_cast<short>(WriteX_Level::DEBUG), {
+    {WriteX_Level::DEBUG, true},
+    {WriteX_Level::INFO, false},
+    {WriteX_Level::WARNING, false},
+    {WriteX_Level::ERROR, false},
+    {WriteX_Level::FATAL, false},
+    {WriteX_Level(1 << 10), false}
+    }},
+    {static_cast<short>(WriteX_Level::INFO), {
+    {WriteX_Level::DEBUG, false},
+    {WriteX_Level::INFO, true},
+    {WriteX_Level::WARNING, false},
+    {WriteX_Level::ERROR, false},
+    {WriteX_Level::FATAL, false},
+    {WriteX_Level(1 << 10), false}
+    }},
+    {static_cast<short>(WriteX_Level::WARNING), {
+    {WriteX_Level::DEBUG, false},
+    {WriteX_Level::INFO, false},
+    {WriteX_Level::WARNING, true},
+    {WriteX_Level::ERROR, false},
+    {WriteX_Level::FATAL, false},
+    {WriteX_Level(1 << 10), false}
+    }},
+    {static_cast<short>(WriteX_Level::ERROR), {
+    {WriteX_Level::DEBUG, false},
+    {WriteX_Level::INFO, false},
+    {WriteX_Level::WARNING, false},
+    {WriteX_Level::ERROR, true},
+    {WriteX_Level::FATAL, false},
+    {WriteX_Level(1 << 10), false}
+    }},
+    {static_cast<short>(WriteX_Level::FATAL), {
+    {WriteX_Level::DEBUG, false},
+    {WriteX_Level::INFO, false},
+    {WriteX_Level::WARNING, false},
+    {WriteX_Level::ERROR, false},
+    {WriteX_Level::FATAL, true},
+    {WriteX_Level(1 << 10), false}
+    }},
+    {1 << 10, {
+    {WriteX_Level::DEBUG, false},
+    {WriteX_Level::INFO, false},
+    {WriteX_Level::WARNING, false},
+    {WriteX_Level::ERROR, false},
+    {WriteX_Level::FATAL, false},
+    {WriteX_Level(1 << 10), true},
+    {WriteX_Level(1 << 11), false},
+    }},
+    {1 << 11, {
+    {WriteX_Level::DEBUG, false},
+    {WriteX_Level::INFO, false},
+    {WriteX_Level::WARNING, false},
+    {WriteX_Level::ERROR, false},
+    {WriteX_Level::FATAL, false},
+    {WriteX_Level(1 << 10), false},
+    {WriteX_Level(1 << 11), true},
+    }}
+  };
 
-  l.setFormat("%F");
-  CHECK(l.getFormat() == "%F");
-  CHECK(l.format(WriteX_Level::DEBUG, "MESSAGE", "FILE", "FUNC", 1) == "FILE");
-  CHECK(l.format(WriteX_Level::INFO, "MESSAGE1", "FILE1", "FUNC1", 2) == "FILE1");
-  CHECK(l.format(WriteX_Level::WARNING, "MESSAGE2", "FILE2", "FUNC2", 3) == "FILE2");
-  CHECK(l.format(WriteX_Level::ERROR, "MESSAGE3", "FILE3", "FUNC3", 4) == "FILE3");
-  CHECK(l.format(WriteX_Level::FATAL, "MESSAGE4", "FILE4", "FUNC4", 5) == "FILE4");
-  CHECK(l.format(WriteX_Level(100), "MESSAGE5", "FILE5", "FUNC5",6) == "FILE5");
-
-  l.setFormat("%f");
-  CHECK(l.getFormat() == "%f");
-  CHECK(l.format(WriteX_Level::DEBUG, "MESSAGE", "FILE", "FUNC", 1) == "FUNC");
-  CHECK(l.format(WriteX_Level::INFO, "MESSAGE1", "FILE1", "FUNC1", 2) == "FUNC1");
-  CHECK(l.format(WriteX_Level::WARNING, "MESSAGE2", "FILE2", "FUNC2", 3) == "FUNC2");
-  CHECK(l.format(WriteX_Level::ERROR, "MESSAGE3", "FILE3", "FUNC3", 4) == "FUNC3");
-  CHECK(l.format(WriteX_Level::FATAL, "MESSAGE4", "FILE4", "FUNC4", 5) == "FUNC4");
-  CHECK(l.format(WriteX_Level(182), "MESSAGE5", "FILE5", "FUNC5",6) == "FUNC5");
-
-  l.setFormat("%l");
-  CHECK(l.getFormat() == "%l");
-  CHECK(l.format(WriteX_Level::DEBUG, "MESSAGE", "FILE", "FUNC", 1) == "1");
-  CHECK(l.format(WriteX_Level::INFO, "MESSAGE1", "FILE1", "FUNC1", 2) == "2");
-  CHECK(l.format(WriteX_Level::WARNING, "MESSAGE2", "FILE2", "FUNC2", 3) == "3");
-  CHECK(l.format(WriteX_Level::ERROR, "MESSAGE3", "FILE3", "FUNC3", 4) == "4");
-  CHECK(l.format(WriteX_Level::FATAL, "MESSAGE4", "FILE4", "FUNC4", 5) == "5");
-  CHECK(l.format(WriteX_Level(940), "MESSAGE5", "FILE5", "FUNC5",6) == "6");
-
-  l.setFormat("%%");
-  CHECK(l.getFormat() == "%%");
-  CHECK(l.format(WriteX_Level::DEBUG, "MESSAGE", "FILE", "FUNC", 1) == "%");
-  CHECK(l.format(WriteX_Level::INFO, "MESSAGE1", "FILE1", "FUNC1", 2) == "%");
-  CHECK(l.format(WriteX_Level::WARNING, "MESSAGE2", "FILE2", "FUNC2", 3) == "%");
-  CHECK(l.format(WriteX_Level::ERROR, "MESSAGE3", "FILE3", "FUNC3", 4) == "%");
-  CHECK(l.format(WriteX_Level::FATAL, "MESSAGE4", "FILE4", "FUNC4", 5) == "%");
-  CHECK(l.format(WriteX_Level(72), "MESSAGE5", "FILE5", "FUNC5",6) == "%");
-
-  l.setFormat("%");
-  CHECK(l.getFormat() == "%");
-  CHECK(l.format(WriteX_Level::DEBUG, "MESSAGE", "FILE", "FUNC", 1) == "%");
-  CHECK(l.format(WriteX_Level::INFO, "MESSAGE1", "FILE1", "FUNC1", 2) == "%");
-  CHECK(l.format(WriteX_Level::WARNING, "MESSAGE2", "FILE2", "FUNC2", 3) == "%");
-  CHECK(l.format(WriteX_Level::ERROR, "MESSAGE3", "FILE3", "FUNC3", 4) == "%");
-  CHECK(l.format(WriteX_Level::FATAL, "MESSAGE4", "FILE4", "FUNC4", 5) == "%");
-  CHECK(l.format(WriteX_Level(90), "MESSAGE5", "FILE5", "FUNC5",6) == "%");
-
-  l.setFormat("[23] {d} {} ()");
-  CHECK(l.getFormat() == "[23] {d} {} ()");
-  CHECK(l.format(WriteX_Level::DEBUG, "MESSAGE", "FILE", "FUNC", 1) == "[23] {d} {} ()");
-  CHECK(l.format(WriteX_Level::INFO, "MESSAGE1", "FILE1", "FUNC1", 2) == "[23] {d} {} ()");
-  CHECK(l.format(WriteX_Level::WARNING, "MESSAGE2", "FILE2", "FUNC2", 3) == "[23] {d} {} ()");
-  CHECK(l.format(WriteX_Level::ERROR, "MESSAGE3", "FILE3", "FUNC3", 4) == "[23] {d} {} ()");
-  CHECK(l.format(WriteX_Level::FATAL, "MESSAGE4", "FILE4", "FUNC4", 5) == "[23] {d} {} ()");
-  CHECK(l.format(WriteX_Level(910), "MESSAGE5", "FILE5", "FUNC5",6) == "[23] {d} {} ()");
-
-  l.setFormat("%0");
-  CHECK(l.getFormat() == "%0");
-  CHECK(l.format(WriteX_Level::DEBUG, "MESSAGE", "FILE", "FUNC", 1) == "%0");
-  CHECK(l.format(WriteX_Level::INFO, "MESSAGE1", "FILE1", "FUNC1", 2) == "%0");
-  CHECK(l.format(WriteX_Level::WARNING, "MESSAGE2", "FILE2", "FUNC2", 3) == "%0");
-  CHECK(l.format(WriteX_Level::ERROR, "MESSAGE3", "FILE3", "FUNC3", 4) == "%0");
-  CHECK(l.format(WriteX_Level::FATAL, "MESSAGE4", "FILE4", "FUNC4", 5) == "%0");
-  CHECK(l.format(WriteX_Level(91), "MESSAGE5", "FILE5", "FUNC5",6) == "%0");
-
-  l.setFormat("");
-  CHECK(l.format(WriteX_Level::DEBUG, "MESSAGE", "FILE", "FUNC", 1) == "");
-  CHECK(l.format(WriteX_Level::INFO, "MESSAGE1", "FILE1", "FUNC1", 2) == "");
-  CHECK(l.format(WriteX_Level::WARNING, "MESSAGE2", "FILE2", "FUNC2", 3) == "");
-  CHECK(l.format(WriteX_Level::ERROR, "MESSAGE3", "FILE3", "FUNC3", 4) == "");
-  CHECK(l.format(WriteX_Level::FATAL, "MESSAGE4", "FILE4", "FUNC4", 5) == "");
-  CHECK(l.format(WriteX_Level(131), "MESSAGE5", "FILE5", "FUNC5", 6) == "");
+  for (const auto& ft : tests) {
+    DYNAMIC_SECTION("Filter = " << ft.filter) {
+      l.setFilter(ft.filter);
+      for (const auto& [lvl, shouldLog] : ft.outcomes) {
+        fake_out.str("");
+        l.log(lvl, "MESSAGE", "FILE", "FUNC", 1);
+        l.flush();
+        std::string ex = shouldLog ? (l.levelToString(lvl)) : "";
+        CHECK(fake_out.str() == ex);
+      }
+    }
+  }
+  
+  l.setFormat(oldFmt);
+  l.setFilter(oldFil);
+  l.switchNewLine();
 }
